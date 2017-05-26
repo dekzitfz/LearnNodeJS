ee29a302be1f507e4a0272101074cec7fd27b3d6
Adi Andrea <dekz.itfz@gmail.com>
2017-05-26 21:36:50 +0800

    create & populate data from db

--- app.js
+++ app.js
@@ -5,28 +5,47 @@ var express     = require("express"),
 
 mongoose.connect('mongodb://localhost/kucing');
 
-var kucingSchema = new mongoose.Schema({
-    nama: String,
-    umur: Number,
-    warna: String
-});
-
-var Kucing = mongoose.model('Kucing', kucingSchema);
-Kucing.create({
-    nama: "anjing",
-    umur: 12,
-    warna: "hitam"
-}, function(error, cat){
-    if(error){
-        console.log(error);
-    }else{
-        console.log(cat);
-    }
-});
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+var myWebSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    description: String
+});
+var Itempost = mongoose.model('Itempost', myWebSchema);
+// Itempost.create(
+//     {
+//         title: "asal post",
+//         image: "http://www.photosforclass.com/download/34482054780",
+//         description: "ini deskripsi"
+//     },
+//     function(error, item){
+//         if(error){console.log(error)}
+//         else{console.log(item)}
+//     }
+// );
+
+// var kucingSchema = new mongoose.Schema({
+//     nama: String,
+//     umur: Number,
+//     warna: String
+// });
+
+// var Kucing = mongoose.model('Kucing', kucingSchema);
+// Kucing.create({
+//     nama: "sapi",
+//     umur: 22,
+//     warna: "sapi"
+// }, function(error, cat){
+//     if(error){
+//         console.log(error);
+//     }else{
+//         console.log(cat);
+//     }
+// });
+
 
 app.get("/", function(request, response){
     //redirect to /home
@@ -35,7 +54,13 @@ app.get("/", function(request, response){
 
 app.get("/home", function(req, res){
     //render index.ejs
-    res.render("index");
+    Itempost.find({}, function(err, allItems){
+        if(err){console.log(err)}
+        else{
+            res.render("index", {items: allItems});
+        }
+    });
+    //res.render("index");
 });
 
 app.get("/home/new", function(req, res){
@@ -43,12 +68,12 @@ app.get("/home/new", function(req, res){
     res.render("new");
 });
 
-app.post("/home", function(req, res){
-    var title   = req.params.title;
-    var image   = req.params.image;
-    var desc    = req.params.description;
+// app.post("/home", function(req, res){
+//     var title   = req.params.title;
+//     var image   = req.params.image;
+//     var desc    = req.params.description;
 
-});
+// });
 
 app.listen(3000, function(){
     console.log("server starting");

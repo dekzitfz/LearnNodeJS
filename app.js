cb1533f3c101b30b90491c57c10fffe1dc09ea70
Adi Andrea <dekz.itfz@gmail.com>
2017-05-26 19:44:06 +0800

    add new form

--- app.js
+++ app.js
@@ -6,16 +6,19 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(request, response){
+    //redirect to /home
     response.redirect("/home");
 });
 
 app.get("/home", function(req, res){
+    //render index.ejs
     res.render("index");
 });
 
-// app.get("/home/new", function(req, res){
-
-// });
+app.get("/home/new", function(req, res){
+    //render new.ejs
+    res.render("new");
+});
 
 app.listen(3000, function(){
     console.log("server starting");

0edf388f19048da2e16dc99dc150b999c8a458c8
Adi Andrea <dekz.itfz@gmail.com>
2017-05-26 20:52:16 +0800

    create schema and model

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 var express     = require("express"),
     bodyParser  = require("body-parser"),
+    mongoose    = require('mongoose'),
     app         = express();
+
+mongoose.connect('mongodb://localhost/kucing');
+
+var kucingSchema = new mongoose.Schema({
+    nama: String,
+    umur: Number,
+    warna: String
+});
+
+var Kucing = mongoose.model('Kucing', kucingSchema);
+Kucing.create({
+    nama: "anjing",
+    umur: 12,
+    warna: "hitam"
+}, function(error, cat){
+    if(error){
+        console.log(error);
+    }else{
+        console.log(cat);
+    }
+});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -25,7 +47,7 @@ app.post("/home", function(req, res){
     var title   = req.params.title;
     var image   = req.params.image;
     var desc    = req.params.description;
-    
+
 });
 
 app.listen(3000, function(){
